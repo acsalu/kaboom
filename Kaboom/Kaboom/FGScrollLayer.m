5f047e686a8e164c753b97ba9113cea14398fe0a
Acsa Lu <acsa0210@gmail.com>
2013-05-14 00:45:02 +0800

    sonata ready

--- Kaboom/Kaboom/FGScrollLayer.m
+++ Kaboom/Kaboom/FGScrollLayer.m
@@ -8,6 +8,7 @@
 
 #import "FGScrollLayer.h"
 #import "GameLayer.h"
+#import "GameSonataLayer.h"
 
 
 enum
@@ -383,7 +384,7 @@ BOOL isMove = NO;
             int selectPage = currentPage - delta;
             selectPage = MIN([layers_ count], selectPage);
             NSLog(@"position: %f, select: %d, current: %d",(touchPoint.y),selectPage,currentPage);   // Start Game!
-            [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5f scene:[GameLayer scene]]];
+            [[CCDirector sharedDirector] replaceScene:[CCTransitionCrossFade transitionWithDuration:0.5f scene:[GameSonataLayer scene]]];
         }
     }
     
--- Kaboom/Kaboom/Song.m
+++ Kaboom/Kaboom/Song.m
@@ -423,7 +423,7 @@
                     @{@"length":@(NOTE_LENGTH_4TH), @"notes":@[@(NOTE_TYPE_REST)]}
                     
                     ];
-    song.interval = 0.5;
+    song.interval = 0.4;
     return song;
 }
 
@@ -449,7 +449,6 @@
         case NOTE_TYPE_BOUNCE_LR2: return @"bounce L2R2";
         case NOTE_TYPE_BOUNCE_RL1: return @"bounce R2L1";
         case NOTE_TYPE_BOUNCE_RL2: return @"bounce R2L2";
-        case NOTE_TYPE_CLAP: return @"clap";
         default: return nil;
     }
 }
